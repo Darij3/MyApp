2bcabd05e64cdd53d5177865f43501110c8e7dd0
Darij3 <34334294+Darij3@users.noreply.github.com>
2024-05-31 11:10:16 +0300

    Add files via upload

---
+++ MyApp.ServiceInterface/MyServices.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using ServiceStack;
+using MyApp.ServiceModel;
+using MyApp.ServiceModel.Models;
+using UserResponse = MyApp.ServiceModel.Models.UserResponse;
+using User = MyApp.ServiceModel.Models.User;
+
+namespace MyApp.ServiceInterface;
+
+public class MyServices : Service
+{
+    public object Any(Hello request)
+    {
+        return new HelloResponse { Result = $"Hello, {request.Name}!" };
+
+    }
+       public object Any(User request)
+        {
+            return new UserResponse { Result = $" {request.id}" };
+    
+            
+        }
+    public object Any(UpdateUser request)
+    {
+        return new UserResponse { Result = $" {request.id}" };
+
+
+    }
+}
